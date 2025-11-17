67a6acd27e7a990eb46230911a3f4c4bef5e8bd1
Biopubler <tor.karlsson@skola.taby.se>
2025-11-17 19:05:28 +0100

    Add files via upload

--- V45/Inloggning-databas.py
+++ V45/Inloggning-databas.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, session, redirect, url_for, request
-import mysql.connector  # installera med "pip install mysql-connector-python" i kommandotolken, ifall du inte redan gjort detta
+import mysql.connector  
 
 app = Flask(__name__)
 app.secret_key = 'hemligtextsträngsomingenkangissa'  # Används för sessionshantering
 
 # skapa databasuppkoppling
-def get_connection(host="localhost", user="root", password=""):
+def get_connection(host="localhost", user="root", password=""): 
     mydb = mysql.connector.connect(
         host=host,
         user=user,
@@ -26,7 +26,7 @@ def index():
         for user in users:
             
             # varje user i loopen är en 4-tippel på formen (username, password, email, id)
-            if request.form['name'] == user[1] and request.form['password'] == user[2]:
+            if request.form['name'] == user[0] and request.form['password'] == user[1]:
                 # i själva verket bör inte lösenord lagras i klartext utan vara krypterade - mer om detta senare i kursen
                 logged_in = True
                 session['user'] = {'username': user[0], 'email': user[2]}
@@ -40,10 +40,11 @@ def index():
 
 @app.route('/login')
 def login():
-    if session:
-        return render_template('login.html', user=session['user'])
+    if 'user' in session:
+      return render_template('login.html', user=session['user'])
     else:
-        return render_template('error.html')
+      return render_template('error.html')
+
 
 @app.route('/annansida')
 def annansida():
@@ -61,7 +62,7 @@ def logout():
 def append():
     if not session: # om man inte är inloggad
         return render_template('error.html')
-    line = f'{session['user']['username']} skrev: {request.form.get('line', '')}'
+    line = f"{session['user']['username']} skrev: {request.form.get('line', '')}"
     with open('meddelanden.txt', 'a', encoding='utf-8') as f:
         f.write(line + '\n')
     return redirect('/annansida')
